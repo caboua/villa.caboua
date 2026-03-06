034e8ee33d7338548cd07e3b9bf13721faca149a
caboua <villa.caboua@gmail.com>
2026-03-06 12:50:40 -0400

    Update send-email.js

--- send-email.js
+++ send-email.js
@@ -1,18 +1,18 @@
-import nodemailer from "nodemailer";
+import nodemailer from "nodemailer"
 
-export default async function sendEmail({ to, subject, text }) {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS // app password Gmail recommandé
+export default async function sendEmail({to,subject,text}){
+  let transporter = nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+      user:process.env.EMAIL_USER,
+      pass:process.env.EMAIL_PASS
     }
-  });
+  })
 
   await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+    from:process.env.EMAIL_USER,
     to,
     subject,
     text
-  });
+  })
 }
