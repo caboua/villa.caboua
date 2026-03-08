2171e7ab1d65b116a91330a2c42fc51eb796ae4a
Villa Caboua <villa.caboua@gmail.com>
2026-03-08 00:48:58 -0400

    add success and cancel pages

---
+++ pages/cancel.js
@@ -0,0 +1,11 @@
+export default function Cancel() {
+  return (
+    <div style={{textAlign:"center", marginTop:"100px", fontFamily:"Arial"}}>
+      <h1>❌ Paiement annulé</h1>
+
+      <p>Votre réservation n'a pas été confirmée.</p>
+
+      <a href="/">Retour au site</a>
+    </div>
+  );
+}
---
+++ pages/success.js
@@ -0,0 +1,9 @@
+export default function Success() {
+  return (
+    <div style={{textAlign:"center", marginTop:"100px"}}>
+      <h1>✅ Réservation confirmée</h1>
+      <p>Merci pour votre réservation à la Villa CABOUA.</p>
+      <a href="/">Retour au site</a>
+    </div>
+  );
+}
